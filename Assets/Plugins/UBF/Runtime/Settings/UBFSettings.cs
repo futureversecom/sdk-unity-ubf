4d37c0dc6cee253f9cd691292c78baf47b310e45
Mike Lee <mike.lee@futureverse.com>
2025-08-08 16:55:09 +1200

    chore: make ubf materials auto load again on validate

--- Assets/Plugins/UBF/Runtime/Settings/UBFSettings.cs
+++ Assets/Plugins/UBF/Runtime/Settings/UBFSettings.cs
@@ -77,38 +77,89 @@ namespace Futureverse.UBF.Runtime.Settings
 
 		private void OnValidate()
 		{
-			// if (_decalOpaque == null)
-			// {
-			// 	_decalOpaque = UnityEngine.Resources.Load("Materials/M_Decal_Opaque") as Material;
-			// }
-			// if (_decalTransparent == null)
-			// {
-			// 	_decalTransparent = UnityEngine.Resources.Load("Materials/M_Decal_Transparent_Alpha") as Material;
-			// }
-			// if (_furOpaque == null)
-			// {
-			// 	_furOpaque = UnityEngine.Resources.Load("Materials/M_Fur_Opaque") as Material;
-			// }
-			// if (_furTransparent == null)
-			// {
-			// 	_furTransparent = UnityEngine.Resources.Load("Materials/M_Fur_Transparent_Alpha") as Material;
-			// }
-			// if (_pbrOpaque == null)
-			// {
-			// 	_pbrOpaque = UnityEngine.Resources.Load("Materials/M_PBR_Opaque") as Material;
-			// }
-			// if (_pbrTransparent == null)
-			// {
-			// 	_pbrTransparent = UnityEngine.Resources.Load("Materials/M_PBR_Transparent_Alpha") as Material;
-			// }
-			// if (_hair == null)
-			// {
-			// 	_hair = UnityEngine.Resources.Load("Materials/M_Hair_Transparent_Alpha_Clipping") as Material;
-			// }
-			// if (_skin == null)
-			// {
-			// 	_skin = UnityEngine.Resources.Load("Materials/M_Skin_Opaque") as Material;
-			// }
+#if UNITY_EDITOR
+			if (_decalOpaque.editorAsset == null)
+			{
+				var materialPath = FindMaterialPath("M_Decal_Opaque");
+				if (materialPath != null)
+				{
+					var material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+					_decalOpaque.SetEditorAsset(material);
+				}
+			}
+			if (_decalTransparent.editorAsset == null)
+			{
+				var materialPath = FindMaterialPath("M_Decal_Transparent_Alpha");
+				if (materialPath != null)
+				{
+					var material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+					_decalTransparent.SetEditorAsset(material);
+				}
+			}
+			if (_furOpaque.editorAsset == null)
+			{
+				var materialPath = FindMaterialPath("M_Fur_Opaque");
+				if (materialPath != null)
+				{
+					var material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+					_furOpaque.SetEditorAsset(material);
+				}
+			}
+			if (_furTransparent.editorAsset == null)
+			{
+				var materialPath = FindMaterialPath("M_Fur_Transparent_Alpha");
+				if (materialPath != null)
+				{
+					var material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+					_furTransparent.SetEditorAsset(material);
+				}
+			}
+			if (_pbrOpaque.editorAsset == null)
+			{
+				var materialPath = FindMaterialPath("M_PBR_Opaque");
+				if (materialPath != null)
+				{
+					var material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+					_pbrOpaque.SetEditorAsset(material);
+				}
+			}
+			if (_pbrTransparent.editorAsset == null)
+			{
+				var materialPath = FindMaterialPath("M_PBR_Transparent_Alpha");
+				if (materialPath != null)
+				{
+					var material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+					_pbrTransparent.SetEditorAsset(material);
+				}
+			}
+			if (_hair.editorAsset == null)
+			{
+				var materialPath = FindMaterialPath("M_Hair_Transparent_Alpha_Clipping");
+				if (materialPath != null)
+				{
+					var material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+					_hair.SetEditorAsset(material);
+				}
+			}
+			if (_skin.editorAsset == null)
+			{
+				var materialPath = FindMaterialPath("M_Skin_Opaque");
+				if (materialPath != null)
+				{
+					var material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+					_skin.SetEditorAsset(material);
+				}
+			}
+			if (_skin02.editorAsset == null)
+			{
+				var materialPath = FindMaterialPath("M_Skin02_Opaque");
+				if (materialPath != null)
+				{
+					var material = AssetDatabase.LoadAssetAtPath<Material>(materialPath);
+					_skin02.SetEditorAsset(material);
+				}
+			}
+#endif
 			if (MeshConfigs == null)
 			{
 				_meshConfigs = new List<MeshConfigEntry>();
@@ -125,6 +176,17 @@ namespace Futureverse.UBF.Runtime.Settings
 #if UNITY_EDITOR
 		internal static SerializedObject GetSerializedSettings()
 			=> new(GetOrCreateSettings());
+		
+		private static string FindMaterialPath(string materialName)
+		{
+			string searchFilter = $"t:Material {materialName}";
+			var packagePath = UnityEditor.PackageManager.PackageInfo.FindForAssetPath($"Packages/com.futureverse.ubf")?.assetPath;
+			packagePath ??= "Assets/Plugins/";
+			
+			var guids = AssetDatabase.FindAssets(searchFilter, new[] { packagePath });
+
+			return guids.Length > 0 ? AssetDatabase.GUIDToAssetPath(guids[0]) : null;
+		}
 #endif
 	}
 }
