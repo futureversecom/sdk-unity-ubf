0a5dd0183e2b4498143f925c5a587c681e4b7a65
Peter Carey <peter.carey@futureverse.com>
2025-08-20 07:32:37 +0800

    feat: add rig, name and filter nodes, add scene object interface

--- Assets/Plugins/UBF/Runtime/Builtin/Scene/FilterSceneObjects.cs
+++ Assets/Plugins/UBF/Runtime/Builtin/Scene/FilterSceneObjects.cs
@@ -7,28 +7,26 @@ using UnityEngine;
 
 namespace Futureverse.UBF.Runtime.Builtin
 {
-    public class FilterSceneComponents : ACustomExecNode
+    public class FilterSceneObjects : ACustomExecNode
     {
-        public FilterSceneComponents(Context context) : base(context) { }
+        public FilterSceneObjects(Context context) : base(context) { }
 
         protected override void ExecuteSync()
         {
-            if (!TryReadArray<SceneComponent>("Scene Components", out var components))
+            if (!TryReadArray<ISceneObject>("Objects", out var components))
             {
-                UbfLogger.LogError("[FilterSceneComponents] Could not find input \"Scene Components\"");
+                UbfLogger.LogError("[FilterObjects] Could not find input \"Scene Components\"");
                 return;
             }
 
             if (!TryRead<string>("Filter", out var filter))
             {
-                UbfLogger.LogError("[FilterSceneComponents] Could not find input \"Filter\"");
+                UbfLogger.LogError("[FilterObjects] Could not find input \"Filter\"");
                 return;
             }
 
-            // TODO implement and filter
-            
             // Do I filter by component specific logic like mesh name? Or by name of scene node?
-            var filtered = components.Where(x => x.GetFilterString().ToLower().Contains(filter.ToLower()));
+            var filtered = components.Where(x => x.GetName().ToLower().Contains(filter.ToLower()));
             WriteOutput("Filtered", filtered.ToArray());
 
         }
---
+++ Assets/Plugins/UBF/Runtime/Builtin/Scene/GetSceneObjectName.cs
@@ -0,0 +1,26 @@
+// Copyright (c) 2025, Futureverse Corporation Limited. All rights reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using Futureverse.UBF.Runtime.Utils;
+using UnityEngine;
+
+namespace Futureverse.UBF.Runtime.Builtin
+{
+    public class GetSceneObjectName : ACustomExecNode
+    {
+        public GetSceneObjectName(Context context) : base(context) { }
+
+        protected override void ExecuteSync()
+        {
+            if (!TryRead<ISceneObject>("Object", out var obj))
+            {
+                UbfLogger.LogError("[GetName] Could not find input \"Object\"");
+                return;
+            }
+
+            
+            WriteOutput("Name", obj?.GetName() ?? string.Empty);
+        }
+    }
+}
