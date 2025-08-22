aa11af90c48891557b3bd538d61c90e027ff517b
AP-ChrisStone <chris.stone@futureverse.com>
2025-08-22 16:27:31 -0700

    chore: Re-implementing math node tests

--- Assets/Tests/PlayModeTests/Standard-2.0.0/TestLoadGraph.cs
+++ Assets/Tests/PlayModeTests/Standard-2.0.0/TestLoadGraph.cs
@@ -1,30 +1,31 @@
-// // Copyright (c) 2025, Futureverse Corporation Limited. All rights reserved.
-//
-// using Futureverse.UBF.Runtime;
-// using Futureverse.UBF.Runtime.Execution;
-// using NUnit.Framework;
-// using UnitTests.PlayModeTests.Utils;
-// using UnityEngine;
-// using UnityEngine.TestTools;
-//
-// public class TestLoadGraph
-// {
-// 	[Test]
-// 	public void LoadGraphNoEntryNode()
-// 	{
-// 		var graphJson = TestGraphBuilder.EmptyGraph(BlueprintVersion.Version);
-// 		Assert.IsTrue(Blueprint.TryLoad("LoadGraphNoEntryNode", graphJson, out var blueprint));
-// 		_ = new BlueprintExecutionTask(blueprint, new ExecutionConfig(null, null));
-// 		LogAssert.Expect(LogType.Error, "[UBF][DLL] Could not execute graph - no Entry node found");
-// 	}
-//
-// 	[Test]
-// 	public void LoadBasicGraph()
-// 	{
-// 		var graphJson = new TestGraphBuilder(BlueprintVersion.Version)
-// 			.AddNode(new DebugLog("dbl", "Hello, World!"))
-// 			.ConnectEntry("dbl")
-// 			.Build();
-// 		Assert.IsTrue(Blueprint.TryLoad("LoadBasicGraph", graphJson, out _));
-// 	}
-// }
+// Copyright (c) 2025, Futureverse Corporation Limited. All rights reserved.
+
+using Futureverse.UBF.Runtime;
+using Futureverse.UBF.Runtime.Execution;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestLoadGraph
+{
+	[Test]
+	public void LoadGraphNoEntryNode()
+	{
+		var graph = TestGraph.EmptyGraph(BlueprintVersion.Version);
+		Assert.IsTrue(Blueprint.TryLoad("LoadGraphNoEntryNode", graph, out var blueprint));
+		_ = new BlueprintExecutionTask(blueprint, new ExecutionConfig(null, null));
+		LogAssert.Expect(LogType.Error, "[UBF][DLL] Could not execute graph - no Entry node found");
+	}
+
+	[Test]
+	public void LoadBasicGraph()
+	{
+		var graph = TestGraph.Create((ref TestGraph g) =>
+		{
+			var debugLog = g.AddNode(new DebugLog("Hello, World!"));
+			g.ConnectEntry(debugLog);
+		});
+		
+		Assert.IsTrue(Blueprint.TryLoad("LoadBasicGraph", graph, out _));
+	}
+}
